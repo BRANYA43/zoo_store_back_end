fe02e5bb7b8b2f101fd613c155b36cf0fc0a9656
BRANYA <tbd2504@gmail.com>
2023-11-25 11:24:05 +0200

    Add returning list with name for get_fields

--- zoo_store/base/model_test.py
+++ zoo_store/base/model_test.py
@@ -7,8 +7,13 @@ class ModelTest(TestCase):
         return model._meta.get_field(field_name)
 
     @staticmethod
-    def get_fields(model) -> list:
-        return model._meta.fields
+    def get_fields(model, *, only_name=False) -> list | list[str]:
+        fields = model._meta.fields
+
+        if only_name:
+            return [field.name for field in fields]
+
+        return fields
 
     @staticmethod
     def get_meta_attr(model, attr_name: str):
