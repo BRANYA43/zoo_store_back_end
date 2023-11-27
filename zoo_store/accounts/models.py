41e85fa11882888dbd7bcadbaab3abf3181f0842
BRANYA <tbd2504@gmail.com>
2023-11-27 11:50:10 +0200

    Add Profile

--- zoo_store/accounts/models.py
+++ zoo_store/accounts/models.py
@@ -1,8 +1,25 @@
 from accounts.managers import UserManager
 from base.mixins import UUIDMixin
+from django.conf import settings
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+
+
+class Profile(UUIDMixin):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=50, null=True, default=None)
+    last_name = models.CharField(max_length=50, null=True, default=None)
+
+    def __str__(self):
+        return self.full_name
+
+    def __repr__(self):
+        return f'<Profile({self.user.email} {self.full_name})>'
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.first_name} {self.last_name}'
 
 
 class User(AbstractBaseUser, PermissionsMixin, UUIDMixin):
--- zoo_store/accounts/tests/test_models.py
+++ zoo_store/accounts/tests/test_models.py
@@ -1,10 +1,51 @@
-from accounts.models import User
+from accounts.models import Profile, User
 from base.test_cases import ModelTestCase
 from django.contrib.auth import get_user_model
 
 
 def create_test_user(email='rick.sanchez@test.com', password='qwe123!@#', **extra_fields):
     return get_user_model().objects.create_user(email=email, password=password, **extra_fields)
+
+
+class ProfileModelTest(ModelTestCase):
+
+    def test_model_has_necessary_fields(self):
+        necessary_fields = ['uuid', 'user', 'first_name', 'last_name']
+        model_fields = self.get_fields(Profile, only_names=True)
+        self.assertFieldNamesEqual(model_fields, necessary_fields)
+
+    def test_uuid_is_primary_key(self):
+        uuid = self.get_field(User, 'uuid')
+        self.assertTrue(uuid.primary_key)
+
+    def test_first_name_is_null_by_default(self):
+        first_name = self.get_field(Profile, 'first_name')
+        self.assertTrue(first_name.null)
+        self.assertIsNone(first_name.default)
+
+    def test_last_name_is_null_by_default(self):
+        last_name = self.get_field(Profile, 'last_name')
+        self.assertTrue(last_name.null)
+        self.assertIsNone(last_name.default)
+
+    def test_profile_has_one_to_one_relation_with_user(self):
+        user = self.get_field(Profile, 'user')
+        self.assertTrue(user.one_to_one)
+        self.assertIs(user.related_model, User)
+
+    def test_profile_is_deleted_after_deleting_user(self):
+        user = create_test_user()
+        Profile.objects.create(user=user)
+
+        user.delete()
+        self.assertEqual(Profile.objects.count(), 0)
+
+    def test_profile_gets_full_name(self):
+        data = {'first_name': 'Rick', 'last_name': 'Sanchez'}
+        profile = Profile(**data)
+        expected_full_name = f"{data['first_name']} {data['last_name']}"
+
+        self.assertEqual(profile.full_name, expected_full_name)
 
 
 class UserModelTest(ModelTestCase):
