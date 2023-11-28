eb672f00be4828f1fd3ce89044f0511c76f43f5a
BRANYA <tbd2504@gmail.com>
2023-11-28 19:17:45 +0200

    Add private user api

--- zoo_store/accounts/views.py
+++ zoo_store/accounts/views.py
@@ -1,5 +1,6 @@
-from accounts import serializers
+from accounts import permissions, serializers
 from django.contrib.auth import get_user_model
+from rest_framework import permissions as rest_permissions
 from rest_framework import viewsets
 
 User = get_user_model()
@@ -9,8 +10,17 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     create_serializer_class = serializers.UserCreateSerializer
     manage_serializer_class = serializers.UserSerializer
+    create_permissions_classes = [rest_permissions.AllowAny]
+    manage_permissions_classes = [permissions.IsOwnerOrStaff]
 
     def get_serializer_class(self):
         if self.action == 'create':
             return self.create_serializer_class
         return self.manage_serializer_class
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = self.create_permissions_classes
+        else:
+            self.permission_classes = self.manage_permissions_classes
+        return super().get_permissions()
