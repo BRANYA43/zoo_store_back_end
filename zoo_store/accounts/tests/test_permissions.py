8d99e3d72ac6fe0fa8adf1f9e80549b110b8ef64
BRANYA <tbd2504@gmail.com>
2023-11-28 19:04:40 +0200

    Add permissions for Owner

---
+++ zoo_store/accounts/tests/test_permissions.py
@@ -0,0 +1,62 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from accounts.permissions import IsOwner, IsOwnerOrStaff
+
+
+class IsOwnerPermissionTest(TestCase):
+    def setUp(self) -> None:
+        self.mock_request = Mock()
+        self.mock_view = Mock()
+        self.mock_obj = Mock()
+        self.permissions = IsOwner()
+
+    def test_permission_allow_read_only_authenticated_or_staff_user(self):
+        self.mock_request.user.is_authenticated = True
+        self.assertTrue(self.permissions.has_permission(self.mock_request, self.mock_view))
+
+        self.mock_request.user.is_staff = True
+        self.assertTrue(self.permissions.has_permission(self.mock_request, self.mock_view))
+
+        self.mock_request.user.is_authenticated = False
+        self.mock_request.user.is_staff = False
+        self.assertFalse(self.permissions.has_permission(self.mock_request, self.mock_view))
+
+    def test_permissions_allow_write_only_owner(self):
+        self.mock_request.user.uuid = 0
+        self.mock_obj.uuid = 0
+        self.assertTrue(self.permissions.has_object_permission(self.mock_request, self.mock_view, self.mock_obj))
+
+        self.mock_request.user.uuid = 1
+        self.assertFalse(self.permissions.has_object_permission(self.mock_request, self.mock_view, self.mock_obj))
+
+
+class IsOwnerOrStaffPermissionTest(TestCase):
+    def setUp(self) -> None:
+        self.mock_request = Mock()
+        self.mock_view = Mock()
+        self.mock_obj = Mock()
+        self.permissions = IsOwnerOrStaff()
+
+    def test_permission_allow_read_only_authenticated_or_staff_user(self):
+        self.mock_request.user.is_authenticated = True
+        self.assertTrue(self.permissions.has_permission(self.mock_request, self.mock_view))
+
+        self.mock_request.user.is_staff = True
+        self.assertTrue(self.permissions.has_permission(self.mock_request, self.mock_view))
+
+        self.mock_request.user.is_authenticated = False
+        self.mock_request.user.is_staff = False
+        self.assertFalse(self.permissions.has_permission(self.mock_request, self.mock_view))
+
+    def test_permissions_allow_write_only_owner_or_admin(self):
+        self.mock_request.user.uuid = 0
+        self.mock_obj.uuid = 0
+        self.assertTrue(self.permissions.has_object_permission(self.mock_request, self.mock_view, self.mock_obj))
+
+        self.mock_request.user.uuid = 1
+        self.mock_request.user.is_staff = True
+        self.assertTrue(self.permissions.has_object_permission(self.mock_request, self.mock_view, self.mock_obj))
+
+        self.mock_request.user.is_staff = False
+        self.assertFalse(self.permissions.has_object_permission(self.mock_request, self.mock_view, self.mock_obj))
