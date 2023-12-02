fa64cd2e8c7d738fe73bc0a4394ee2acb6060424
BRANYA <tbd2504@gmail.com>
2023-12-02 15:34:40 +0200

    Refactor tests for token create and refresh view sets

--- zoo_store/accounts/tests/test_public_api/test_token_api.py
+++ zoo_store/accounts/tests/test_public_api/test_token_api.py
@@ -4,33 +4,41 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 
 
-class TokenCreateTest(ViewSetTestCase):
+class TokenCreateAndRefreshTest(ViewSetTestCase):
     
     def setUp(self) -> None:
         self.data = {
             'email': 'rick.sanchez@test.com',
             'password': 'qwe123!@#',
         }
-        self.url = reverse('token_obtain_pair')
-        self.r_url = reverse('token_refresh')
+        self.create_url = reverse('token_obtain_pair')
+        self.refresh_url = reverse('token_refresh')
     
     def test_create_token_for_unregister_user(self):
-        response = self.client.post(self.url, self.data)
+        response = self.client.post(self.create_url, self.data)
         
         self.assertStatusCodeEqual(response, status.HTTP_401_UNAUTHORIZED)
         self.assertUserIs(response, 'anonymous')
     
     def test_create_token_for_registered_user(self):
-        create_test_user('rick.sanchez@test.com', 'qwe123!@#')
+        create_test_user(**self.data)
         
-        response = self.client.post(self.url, self.data)
+        response = self.client.post(self.create_url, self.data)
         
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('access', response.data)
-        self.assertIn('refresh', response.data)
+        self.assertStatusCodeEqual(response, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data.get('access'))
 
-        refresh_token = response.data.get('refresh')
-        if refresh_token:
-            refresh_response = self.client.post(self.r_url, {'refresh': refresh_token})
-            self.assertEqual(refresh_response.status_code, status.HTTP_200_OK)
-            self.assertIn('access', refresh_response.data)
+    def test_refresh_token_for_registered_user(self):
+        create_test_user(**self.data)
+
+        created_response = self.client.post(self.create_url, self.data)
+        refresh_token = created_response.data.get('refresh')
+
+        self.assertIsNotNone(refresh_token)
+
+        refresh_response = self.client.post(self.refresh_url, {'refresh': refresh_token})
+
+        self.assertStatusCodeEqual(refresh_response, status.HTTP_200_OK)
+        self.assertIn('access', refresh_response.data)
+
+
